3920702f634bc849435dbbe3a8918e426664a6f5
Zlash65 <zarrar65@gmail.com>
2017-06-09 17:19:26 +0530

    Added new controller to handle Posts page

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -1,6 +1,13 @@
 var myApp = angular.module('myApp', []);
 
-myApp.controller('AppCtrl', ['$scope', '$http', function($scope, $http){
+myApp.factory('sharingData', [ function(){
+	var o = {};
+		// posts: []
+	// };
+	return o;
+}]);
+
+myApp.controller('AppCtrl', ['$scope', '$http', 'sharingData', '$rootScope', function($scope, $http, sharingData, $rootScope){
 
 	var refresh = function(){
 		$http.get('/fredpost').success(function(response){
@@ -13,6 +20,8 @@ myApp.controller('AppCtrl', ['$scope', '$http', function($scope, $http){
 	$scope.addPost = function(){
 		if(!$scope.post.title || $scope.post.title === '') { return; }
 		$scope.post.upvotes = 0;
+		$scope.post.comments = {author: 'Joe', body: 'Cool post!', upvotes: 0};
+			// {author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
 		$http.post('/fredpost', $scope.post).success(function(response){
 			console.log("Success");
 			refresh();
@@ -30,5 +39,22 @@ myApp.controller('AppCtrl', ['$scope', '$http', function($scope, $http){
 		});
 	};
 
+	$scope.postRoute = function(x){
+
+		$rootScope.$emit("sharing", "blah");
+
+		var newWindows = window.open("posts.html","_self");
+
+	};
+
+}]);
+
+myApp.controller('PostsCtrl', ['$scope', 'sharingData', '$rootScope', function($scope, sharingData, $rootScope){
+
+	$rootScope.$on('sharing', function(event, data){
+        console.log('blah');
+    });
+
+    console.log('blah');
 
 }]);
