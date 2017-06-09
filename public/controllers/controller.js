3ad466d58f60eec9164094affd937f8225fde239
Zlash65 <zarrar65@gmail.com>
2017-06-09 20:18:18 +0530

    Updated to handle controllers for both pages

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -1,60 +1,79 @@
 var myApp = angular.module('myApp', []);
 
-myApp.factory('sharingData', [ function(){
-	var o = {};
-		// posts: []
-	// };
-	return o;
-}]);
+myApp.controller('AppCtrl', ['$scope', '$http', '$window', function($scope, $http, $window){
 
-myApp.controller('AppCtrl', ['$scope', '$http', 'sharingData', '$rootScope', function($scope, $http, sharingData, $rootScope){
-
+	var setUsername = function(){
+		if(!($window.sessionStorage.getItem("username")))
+			$window.sessionStorage.setItem( "username", prompt("Enter a user name: ", "user"));
+	};
+	setUsername();
+	
 	var refresh = function(){
 		$http.get('/fredpost').success(function(response){
 			$scope.posts = response;
 		});
 	};
-
 	refresh();
 
 	$scope.addPost = function(){
 		if(!$scope.post.title || $scope.post.title === '') { return; }
 		$scope.post.upvotes = 0;
-		$scope.post.comments = {author: 'Joe', body: 'Cool post!', upvotes: 0};
-			// {author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
+		$scope.post.comments = [];
 		$http.post('/fredpost', $scope.post).success(function(response){
-			console.log("Success");
-			refresh();
 		});
-
+		refresh();
 		$scope.title = ''; 
 		$scope.link = '';
 	};
 
 	$scope.incrementUpvotes = function(post){
 		post.upvotes += 1;
-		console.log(post._id);
-		$http.put('fredpost/' + post._id, post).success(function(response){
-			console.log("Success");
+		$http.put('/fredpost/' + post._id, post).success(function(response){
 		});
 	};
 
 	$scope.postRoute = function(x){
-
-		$rootScope.$emit("sharing", "blah");
-
+		$window.sessionStorage.setItem("data", JSON.stringify(x));
+		console.log($window.sessionStorage.getItem("data"));
 		var newWindows = window.open("posts.html","_self");
-
 	};
-
 }]);
 
-myApp.controller('PostsCtrl', ['$scope', 'sharingData', '$rootScope', function($scope, sharingData, $rootScope){
+myApp.controller('PostsCtrl', ['$scope', '$window', '$http', function($scope, $window, $http){
 
-	$rootScope.$on('sharing', function(event, data){
-        console.log('blah');
-    });
+	var x = JSON.parse($window.sessionStorage.getItem("data"));
+	$scope.post = x;
 
-    console.log('blah');
+	var refresh = function(){
+		$http.get('/fredpost/posts/' + $scope.post._id).success(function(response){
+			$scope.post = response;
+			$window.sessionStorage.setItem("data", JSON.stringify(response));
+		});
+	};
+	refresh();
 
+	$scope.addComment = function(){
+		if($scope.body === '') { return; }
+		
+		$scope.post.comments.push({
+			body: $scope.body,
+			author: $window.sessionStorage.getItem("username"),
+			upvotes: 0
+		});
+
+		$http.put('/fredpost/posts/' + $scope.post._id, $scope.post).success(function(response){
+			// $window.sessionStorage.setItem("data", JSON.stringify(response));
+		});
+		$scope.body = '';
+		refresh();
+	};
+
+	$scope.incrementUpvotes = function(comment){
+		comment.upvotes += 1;
+
+		$http.put('/fredpost/posts/' + $scope.post._id, $scope.post).success(function(response){
+			// $window.sessionStorage.setItem("data", JSON.stringify(response));
+		});	
+		refresh();
+	};
 }]);
